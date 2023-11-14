9946e8a7d441ea9c2dca281a3168132b4be2a108
danischiavi <danielaschivinato@gmail.com>
2023-11-14 16:12:51 +1300

    updated slicing-database.sh to include short-ncrna

--- bin/slicing-database.sh
+++ bin/slicing-database.sh
@@ -9,85 +9,92 @@
 #
 # Input:
 #        $1 is the downloaded fasta file from RNAcentral containing the lncRNAs. 
-#        $2 is the downloaded chromosome coordinates file from RNAcentral for all ncRNAs in the database. Eg: Homo_sapiens.GRCh38.bed
-
+#        $2 is the downloaded fasta file from RNAcentral containing the short-ncRNAs.
+#        $3 is the downloaded chromosome coordinates file from RNAcentral for all ncRNAs in the database. Eg: Homo_sapiens.GRCh38.bed
 
 
 ###########################################################################################################################
 
 ######## General setup
 
-###Rename parameters for better readability  #paths to files (here just for easy access)
-lncrna_fasta=/Volumes/archive/gardnerlab/helenacooper/features-of-function/data/lncrna/rnacentral/long-ncrna-rnacentral.fa 
-chr_coord_ncrna=/Volumes/archive/gardnerlab/features_of_function/data/homo_sapiens.GRCh38.bed
+###Rename parameters for better readability  #paths to files (here just for easy access) --> set them on $PATH!
+#lncrna_fasta=/Volumes/archive/gardnerlab/helenacooper/features-of-function/data/lncrna/rnacentral/long-ncrna-rnacentral.fa 
+#short_ncrna_fasta=/Volumes/archive/gardnerlab/helenacooper/features-of-function/data/lncrna/rnacentral/short-ncrna-rnacentral.fa 
+#chr_coord_ncrna=/Volumes/archive/gardnerlab/features_of_function/data/homo_sapiens.GRCh38.bed
 
+
+lncrna_fasta="$1"
+short_ncrna_fasta="$2"
+chr_coord_ncrna="$3"
+
+######## Convert input from FASTA to CSV file for easier parsability
+## check if files exist 
+if [ ! -f "$lncrna_fasta" ] || [ ! -f "$short_ncrna_fasta" ] || [ ! -f "$chr_coord_ncrna" ] 
+then
+    echo "Initial data files do not exist"
+    break 
+else
+    :
+fi
+
+fasta_formatter -i "$lncrna_fasta" -o "$d-converted-lncrna.csv" -t 
+fasta_formatter -i "$short_ncrna_fasta" -o "$d-converted-short-ncrna.csv" -t
 
 # set -xv
 IFS=$'\n'
 d=$(date +%y%m%d) 
 
-#######Declare function
 
-function my_function() {
+## sequence length limits
+lower_limit='75'
+upper_limit='3000'
 
+####### Declare function
+
+function set_variables() { 
     local id=$1
     local chr_coord=$2
     local seq_csv=$3
-    local n=$4
     
+    seq=$( grep -m 1 "$id" "$seq_csv" | cut -f 2 )
     meta=$( grep -m 1 "$id" "$chr_coord" )
     chr=$( echo "$meta" | cut -f 1 )
-    true_start=$( echo "$meta" | cut -f 2 )  
-    true_end=$( echo "$meta" | cut -f 3 )
-    len=$( echo "$meta" | cut -f 11 | cut -d ',' -f 1 )
-    end=$(( $true_start + $len ))
-    exon_count=$( echo "$meta" | cut -f 10 )
-    seq=$( grep -m 1 "$id" "$seq_csv" | cut -f 2 )
+    status='pass'
         
     if [ -z "$seq" ]   # If no sequence available for lncRNA, remove
     then
-        :
-
+        status='not-pass'
+        return
 	elif [ $chr == 'chrM' ] || [ $chr == 'chrY' ]  # Removes ncRNA from mitochondria and Y chromosome
     then
-        :
-	
-    elif [ $exon_count -lt "4" ]   # Need at least four exons
-    then
-        :
-	    
+        status='not-pass'
+        return
     else
-        len_two=$( echo $meta | cut -f 11 | cut -d ',' -f 2 ) #length exon2 
-        len_three=$( echo $meta | cut -f 11 | cut -d ',' -f 3 ) ##length exon3
-
-        if [ "$len_two" -lt "3000" ] || [ "$len_three" -lt "3000" ]   # exon length cannot be greater than 3000 bp #D: ADD minimum length: -gt "75"
-        then
-            echo $meta >> $d-$n-sliced-database.bed
-        else
-            :
-        fi
+        :
+    
     fi
 }
 
  
-######## Convert input from FASTA to CSV file for easier parsability
-fasta_formatter -i "$lncrna_fasta" -o "$d-converted-lncrna.csv" -t 
-
-
 ######## Obtain chromosome coordinates for each functional ncRNA
 
 ####Define assosiative array for more efficient searching
 
 IDs_ncrna=()  #to store the IDs of all ncRNA from RNAcentral database 
-IDs_long_ncrna=()   #to store the IDs of long-ncRNA from RNAcentral database
+IDs_lncrna=()   #to store the IDs of long-ncRNA from RNAcentral database
+IDs_short_ncrna=()
 
-
-##Populate the arrays
+##Populate arrays
 
 while read -r line; do
-    ID=$( echo "$line" | cut -f 1 | cut -c1-18 )
-    IDs_long_ncrna+=("$ID")
+    ID=$( echo "$line" | cut -f 1 | cut -c1-18 )  #D: check how long the field is 
+    IDs_lncrna+=("$ID")
 done < "$d-converted-lncrna.csv"
+
+while read -r line; do
+    ID=$( echo "$line" | cut -f 1 | cut -c1-18 ) 
+    IDs_short_ncrna+=("$ID")
+done < "$d-converted-short.csv"
 
 while read -r line; do 
     IdField=$( echo "$line" | cut -f 4 | cut -c1-18 )
@@ -98,14 +105,52 @@ done < "$chr_coord_ncrna"
 ####Searching for the Id on the RNAcentral database
 for id in "${IDs_ncrna[@]}"; do
 
-    id_var="$id"
-
-    if [[ "${IDs_long_ncrna[@]}" =~ "$id_var" ]]
+    if [[ "${IDs_lncrna[@]}" =~ "$id" ]]  #D: change order? 
     then
-        my_function "$id_var" "$chr_coord_ncrna" "$d-converted-lncrna.csv" "lncrna"
+        set_variables "$id" "$chr_coord_ncrna" "$d-converted-lncrna.csv" 
+        if [ "$status" != 'not-pass' ]
+        then 
+            exon_count=$( echo "$meta" | cut -f 10 )
+            if [ "$exon_count" -lt "4" ]
+            then
+                :
+        
+            else 
+                len_two=$( echo $meta | cut -f 11 | cut -d ',' -f 2 ) #length exon2 
+                len_three=$( echo $meta | cut -f 11 | cut -d ',' -f 3 ) ##length exon3
 
-    else
-        :
-    fi 
+                if [ "$len_two" -ge "$lower_limit" ] && [ "$len_two" -le "$upper_limit" ] || [ "$len_three" -ge "$lower_limit" ] && [ "$len_three" -le "$upper_limit" ]   # exon length cannot be greater than 3000 bp #D: ADD minimum length: -gt "75"
+                then
+                    echo "$meta" >> $d-lncrna-sliced-database-TEST.bed
+                else
+                    :
+                fi
+            fi
+        fi
+
+
+    elif [[ "${IDs_short_ncrna[@]}" =~ "$id" ]]
+    then
+        set_variables "$id" "$chr_coord_ncrna" "$d-converted-short-ncrna.csv" 
+        if [ "$status" != 'not-pass' ]
+        then
+            len=$( echo "$meta" | cut -f 11 | cut -d ',' -f 1 )
+            if [ "$len" -ge "$lower_limit" ] && [ "$len" -le "$upper_limit" ]  
+            then
+                echo "$meta" >> $d-short-ncrna-sliced-database-TEST.bed
+        
+            else
+                :
+        
+            fi 
+        fi
+    fi
 
 done
+
+
+
+
+
+
+
