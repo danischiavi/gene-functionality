451aba19e6cbd1e01f6efbcbebe6a1c606090ea9
danischiavi <danielaschivinato@gmail.com>
2023-12-31 14:56:27 +1300

    change GERP score (from bedtools to bigWigSummary)

--- bin/feature-generation-splitting/seq-conservation-features.sh
+++ bin/feature-generation-splitting/seq-conservation-features.sh
@@ -20,9 +20,9 @@
 
 ## bigWigSummary
 
-if find $additional_folder/ -executable -type f | grep -q bigWigSummary
+if find bin/ -executable -type f | grep -q bigWigSummary
 then
-    bigWigSummary_exe=$additional_folder/bigWigSummary
+    bigWigSummary_exe=bin/bigWigSummary
 elif command -v bigWigSummary &> /dev/null
 then
     bigWigSummary_exe=bigWigSummary
@@ -31,36 +31,36 @@ else
     exit 1
 fi
 
-## Local bigwig file of UCSC phyloP scores for BigWigSummary
+## Local bigwig file of UCSC 100way phyloP scores for BigWigSummary
 
-if [ -f $additional_folder/hg38.phyloP100way.bw ] 
+if [ -f data/raw/hg38.phyloP100way.bw ] ## UPDATE
 then
-    phylo_bw=$additional_folder/hg38.phyloP100way.bw
+    phylo_bw=data/raw/hg38.phyloP100way.bw
 else
     until [ -f $phylo_bw ] ; do read -p "Please enter custom name of hg38 conservation scoring by phyloP (bigWig): " phylo ; phylo_bw=$additional_folder/$phylo ; echo ; done
 fi
 
 ##Local bigwig file of UCSC phastCons scores for BigWigSummary
 
-if [ -f $additional_folder/hg38.phastCons100way.bw ] 
+if [ -f $additional_folder/hg38.phastCons100way.bw ] ### update
 then
     phast_bw=$additional_folder/hg38.phastCons100way.bw 
 else
     until [ -f $phast_bw ] ; do read -p "Please enter custom name of hg38 conservation scoring by phastCons (bigWig): " phast ; phast_bw=$additional_folder/$phast ; echo ; done
 fi
 
-##Local bigwig file of UCSC phastCons scores for BigWigSummary
+##Local bigwig file of UCSC 241way phastP scores for BigWigSummary
 
-if [ -f $additional_folder/hg38.phastCons100way.bw ] 
+if [ -f data/raw/241-mammalian-2020v2.bigWig ]  
 then
-    zoonomia_phyloP_bw=./data/241-mammalian-2020v2.bigWig
+    zoonomia_phyloP_bw=data/raw/241-mammalian-2020v2.bigWig
 else
     until [ -f $zoonomia_phyloP_bw ] ; do read -p "Please enter custom name of hg38 conservation scoring by phastCons (bigWig): " phast ; phast_bw=$additional_folder/$phast ; echo ; done
 fi
 
-##Local bigwig file of 111 mammal GERP scores for BigWigSummary
+##Local bigwig file of 111 mammal GERP scores for BigWigSummary ## UPDATE
 
-if [ -d $additional_folder/gerp-mammals-index/ ] && [ -f $additional_folder/gerp-mammals-index/gerp1.bed.gz ] 
+if [ -d data/raw/gerp-mammals-index/ ] && [ -f $additional_folder/gerp-mammals-index/gerp1.bed.gz ] 
 then
     mammals_bed=$additional_folder/gerp-mammals-index
 else
@@ -68,18 +68,6 @@ else
 $additional_folder/$mammal ; echo ; done
 fi
 
-######## bedtools
-
-if find $additional_folder/ -executable -type f | grep -q bedtools
-then
-    bedtools_exe=$additional_folder/bedtools
-elif command -v bedtools &> /dev/null
-then
-    bedtools_exe=bedtools
-else
-    echo Please check that bedtools is installed.
-    exit 1
-fi
 
 ############################################################################################################################
 
@@ -87,16 +75,11 @@ fi
 
 ############################################################################################################################
 
-echo Zoonomia-MeanPhyloP,Zoonomia-MaxPhyloP,MeanPhyloP,MaxPhyloP,MeanPhastCons,MaxPhastCons > ./data/$name_set-conservation.csv
-echo mammals_mean_gerp,mammals_max_gerp > ./data/$name_set-gerp.csv
-
-var="$first_rna_id"
+echo Zoonomia-MeanPhyloP,Zoonomia-MaxPhyloP,MeanPhyloP,MaxPhyloP,MeanPhastCons,MaxPhastCons > data/conservation-$name.csv
+echo GERP_mean,GERP_max_gerp > data/gerp-$name.csv
 
 while IFS=',' read -r _ _ chr start end _
 do
-    #echo '#####################RNA'$var'#####################' >> errors.log #D:i think I dont need the var in this section. Maybe another to append to error.log?
-    
-    
     ######## Obtain phastCons (pc) and phyloP (pp) values for each set of chromosome coordinates
 	echo "mean_pp = $bigWigSummary_exe -type=mean $phylo_bw $chr $start $end 1 2>&1" >> errors.log
         mean_pp=$( $bigWigSummary_exe -type=mean $phylo_bw $chr $start $end 1 2>&1 )
@@ -106,13 +89,16 @@ do
 	echo "max_pc = $bigWigSummary_exe -type=max $phast_bw $chr $start $end 1 2>&1" >> errors.log
 	
     ######## Extract PhyloP (pp) values from 241-way mammalian alignment for each set of chromosome coordinates
-    zoonomia_mean_pp=$( $bigWigSummary_exe -type=mean $zoonomia_phylo_bw $chr $start $end 1 2>&1 )
-    zoonomia_max_pp=$(   $bigWigSummary_exe -type=max $zoonomia_phylo_bw $chr $start $end 1 2>&1 )
+    zoonomia_mean=$( $bigWigSummary_exe -type=mean $zoonomia_phylo_bw $chr $start $end 1 2>&1 )
+    zoonomia_max=$(   $bigWigSummary_exe -type=max $zoonomia_phylo_bw $chr $start $end 1 2>&1 )
     
+    ######## Obtain GERP Scores for each set of chromosome coordinates
+    gerp_mean=$( $bigWigSummary_exe -type=mean $gerp_bw $chr $start $end 1 2>&1 )
+    gerp_max=$(   $bigWigSummary_exe -type=max $gerp_bw $chr $start $end 1 2>&1 )
 
     ######## Convert any missing data to NAs (mean = mn, max = mx, phyloP = p, phastCons = c) --> D: declare a short function for this
         test_mnp=$( echo $mean_pp | wc -w )
-        if [[ "$test_mnp" -eq "1" ]]; then : ; else mean_pp=NA ; fi
+            if [[ "$test_mnp" -eq "1" ]]; then : ; else mean_pp=NA ; fi
 	
         test_mxp=$( echo $max_pp | wc -w )
         if [[ "$test_mxp" -eq "1" ]]; then : ; else max_pp=NA ; fi
@@ -123,34 +109,21 @@ do
         test_mxc=$( echo $max_pc | wc -w )
         if [[ "$test_mxc" -eq "1" ]]; then : ; else max_pc=NA; fi
 	
-        test_mnc=$( echo $zoonomia_mean_pp | wc -w )
+        test_mnc=$( echo $zoonomia_mean | wc -w )
         if [[ "$test_mnc" -eq "1" ]]; then : ; else mean_pc=NA ; fi
 	
-        test_mxc=$( echo $zoonomia_max_pp | wc -w )
+        test_mxc=$( echo $zoonomia_max | wc -w )
         if [[ "$test_mxc" -eq "1" ]]; then : ; else max_pc=NA ; fi
 
-    echo $zoonomia_mean_pp,$zoonomia_max_pp,$mean_pp,$max_pp,$mean_pc,$max_pc >> ./data/$name_dataset-conservation.csv
-    
-    ######## Obtain GERP Scores for each set of chromosome coordinates
-    ## Format data for bedtools
-    #need to strip "chr" from input.bed & from "gerp$chr.bed.gz"
-    chromo=$( echo $chr | tr -d 'chr' )               
-    echo -e "$chromo\t$start\t$end" > ./data/input-gerp.bed   # Input for bedtools (GERP) -e option for tab recognition
-	echo "[chr:$chr] [chromo:$chromo] [start:$start] [end:$end]" >> errors.log 
-
-	echo "$bedtools_exe map -a ./data/input-gerp.bed -b $mammals_bed/gerp$chromo.bed.gz -c 4,4 -o mean,max -sorted 2>>/dev/null" >> errors.log  #  map: Apply a function to a column for each overlapping interval
-	mammal_output=$( $bedtools_exe map -a ./data/input-gerp.bed -b $mammals_bed/gerp$chromo.bed.gz -c 4,4 -o mean,max -sorted 2>> errors.log)     # -c Specify the column from the B file to map onto intervals in A 
-    IFS=$'\t' read -r _ _ _ mean_mammal max_mammal <<< "$mammal_output"     # Mean and Max GERP Score
+        test_mnc=$( echo $gerp_mean | wc -w )
+        if [[ "$test_mnc" -eq "1" ]]; then : ; else gerp_mean=NA ; fi
 	
-    ######## Convert any missing data to NAs
-    if [[ "$mean_mammal" == "." ]]; then mean_mammal=NA ; else : ; fi
-    if [[ "$max_mammal" == "." ]]; then max_mammal=NA ; else : ; fi
+        test_mxc=$( echo $gerp_max | wc -w )
+        if [[ "$test_mxc" -eq "1" ]]; then : ; else gerp_max=NA ; fi
     
-    #CHECK IF THIS ONES ARE NEEDED:     
-    if [ -z "$mean_mammal" ]; then mean_mammal='NA'; else :; fi
-    if [ -z "$max_mammal" ]; then max_mammal='NA'; else :; fi
-	
-    echo $mean_mammal,$max_mammal >> ./data/$name_dataset-gerp.csv
+    echo $zoonomia_mean,$zoonomia_max,$mean_pp,$max_pp,$mean_pc,$max_pc >> data/conservation-$name.csv
+    echo $gerp_mean,$gerp_max >> data/gerp-$name.csv
  	
 done < "$initial_data"
+
 
