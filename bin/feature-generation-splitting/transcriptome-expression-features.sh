208a067612509e770298c49dd813adfc1b23f2c0
danischiavi <danielaschivinato@gmail.com>
2024-01-27 16:06:17 +1300

    small format changes

--- bin/feature-generation-splitting/transcriptome-expression-features.sh
+++ bin/feature-generation-splitting/transcriptome-expression-features.sh
@@ -72,7 +72,7 @@ echo RPKM_$sample_type,MRD_$sample_type > data/$sample_type-rnaseq-$name.csv
 
 ######## Define location of downloaded ENCODE RNAseq data
 encode_data=$encode_folder/ENCODE-$sample_type/*.bam
-total_read_file=$encode_folder/ENCODE-$sample_type/total-read-count.txt ##BROKEN FILE!
+total_read_file=$encode_folder/ENCODE-$sample_type/total-read-count.txt 
 
 while IFS=',' read -r _ _ chr start end _
 do
@@ -105,7 +105,7 @@ do
     [ -z "$max_depth" ] && max_depth=0                                                            # Record MRD as zero rather than blank
     if [ -z "$count_max" ] ; then count_max=NA ; max_depth=NA ; else : ; fi                       # But if no transcription whatsoever, set everything to NA 
 
-    echo $count_max,$max_depth >> rnaseq-$name.csv
+    echo $count_max,$max_depth >> data/$sample_type-rnaseq-$name.csv
 
 done < $initial_data
 
