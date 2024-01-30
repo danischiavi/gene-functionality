fb1e0026c6689e2734b81a7ca3e7eff68e01824c
danischiavi <danielaschivinato@gmail.com>
2024-01-30 15:43:11 +1300

    update for snakemake

--- bin/feature-generation-splitting/transcriptome-expression-features.sh
+++ bin/feature-generation-splitting/transcriptome-expression-features.sh
@@ -16,98 +16,66 @@
 
 ############################################################################################################################
 
-#### Checks 
+initial_data=$1
+mkdir -p data/transcriptome
+output_directory=data/transcriptome
 
-## samtools
-
-if find $additional_folder/ -executable -type f | grep -q samtools
-then
-    samtools_exe=$additional_folder/samtools
-elif command -v samtools &> /dev/null
-then
-    samtools_exe=samtools
-else
-    echo Please check that samtools is installed.
-    exit 1
-fi
-
-## ENCODE Tissue Total & Small RNASeq BAM files for samtools
-
-tissue=$( ls $additional_folder/ENCODE-tissue/*.bam | wc -l )
-tissue_bam=$( ls $additional_folder/ENCODE-tissue/*.bam.bai | wc -l )
-tissue_max=$( cat $additional_folder/ENCODE-tissue/total-read-count.txt | wc -l )
-
-if [[ $tissue -eq $tissue_bam ]] && [ -f $additional_folder/ENCODE-tissue/total-read-count.txt ] && [[ $tissue -eq $tissue_max ]]
-then
-    :
-else
-    echo "Please confirm you have Tissue RNASeq BAM files downloaded from ENCODE."
-    echo
-    exit 1 
-fi
-
-## ENCODE Primary Cell Total & Small RNA-seq BAM files for samtools
-
-primary_cell=$( ls $additional_folder/ENCODE-primary-cell/*.bam | wc -l )
-primary_bam=$( ls $additional_folder/ENCODE-primary-cell/*.bam.bai | wc -l )
-primary_max=$( ls $additional_folder/ENCODE-primary-cell/total-read-count.txt | wc -l )
-
-if [[ $primary_cell -eq $primary_bam ]] && [ -f $additional_folder/ENCODE-primary-cell/total-read-count.txt ] && [[ $primary_cell -eq $primary_max ]]
-then
-    :
-else
-    echo "Please confirm you have Primary Cell RNASeq BAM files downloaded from ENCODE."
-    echo
-    exit 1
-fi
-
-encode_folder=$1
+# Variables
+encode_folder=/Volumes/archive/userdata/student_users/danielaschiavinato/dani-scratch/features-of-function-data
+samtools_exe=samtools
 
 #### Function declaration 
 run_encode() {
 
-sample_type=$1
+    local sample_type=$1
 
-echo RPKM_$sample_type,MRD_$sample_type > data/$sample_type-rnaseq-$name.csv
+    output_file="${output_directory}/$(basename "${initial_data%.*}" | sed 's/dataset//')"$sample_type".csv"   # Define name and directory for output file
+    echo RPKM_$sample_type,MRD_$sample_type > $output_file
 
-######## Define location of downloaded ENCODE RNAseq data
-encode_data=$encode_folder/ENCODE-$sample_type/*.bam
-total_read_file=$encode_folder/ENCODE-$sample_type/total-read-count.txt 
+    ######## Define location of downloaded ENCODE RNAseq data
+    encode_data=$encode_folder/ENCODE-$sample_type/*.bam
+    total_read_file=$encode_folder/ENCODE-$sample_type/total-read-count.txt 
 
-while IFS=',' read -r _ _ chr start end _
-do
-    length=$(( $end-$start ))
-    length_kb=$( echo "scale=2; $length/1000" | bc )                                             # Convert to Kb
-    input_samtools=$chr':'$start'-'$end
+    {
+        read
+        while IFS=',' read -r _ _ chr start end _; do
     
-    ######## Record RPKM and MRD across all RNAseq files
-    count_max=0                                                                                  # To record maximum RPKM and MRD observed
-    max_depth=0                                                                                  # To record maximum MRD observed
+            length=$(( $end-$start ))
+            length_kb=$( echo "scale=2; $length/1000" | bc )                                             # Convert to Kb
+            input_samtools=$chr':'$start'-'$end
+    
+        ######## Record RPKM and MRD across all RNAseq files
+            count_max=0                                                                                  # To record maximum RPKM and MRD observed
+            max_depth=0                                                                                  # To record maximum MRD observed
 
-    for file in $encode_data                                                                     # Loop through all RNAseq datasets
-    do 
-        read=$( $samtools_exe view -c $file $input_samtools 2>>errors.log )                      # Number of reads for sequence of interest
-        file_name=$( echo $file | rev | cut -d '/' -f 1 | rev )                                  # ID for RNAseq dataset
-        total_read=$( grep "$file_name" $total_read_file | cut -d ',' -f 2 )                     # Obtain total number of reads for specific RNAseq dataset (precomputed to save time)
-        scaling=$(   echo "$total_read/1000000" | bc )                                           # Create the "by per million" scaling 
-        rpm=$(   echo "scale=2; $read/$scaling" | bc )                                           # Reads per million
-        rpkm=$( echo "scale=2; $rpm/$length_kb" | bc )                                           # Final RPKM calculation
+            for file in $encode_data; do                                                                     # Loop through all RNAseq datasets
+     
+                read=$( $samtools_exe view -c $file $input_samtools 2>>errors.log )                      # Number of reads for sequence of interest
+                file_name=$( echo $file | rev | cut -d '/' -f 1 | rev )                                  # ID for RNAseq dataset
+                total_read=$( grep "$file_name" $total_read_file | cut -d ',' -f 2 )                     # Obtain total number of reads for specific RNAseq dataset (precomputed to save time)
+                scaling=$(   echo "$total_read/1000000" | bc )                                           # Create the "by per million" scaling 
+                rpm=$(   echo "scale=2; $read/$scaling" | bc )                                           # Reads per million
+                rpkm=$( echo "scale=2; $rpm/$length_kb" | bc )                                           # Final RPKM calculation
         
-        # If RPKM is greater than previously recorded, then overwrite the variable.
-        if (( $(echo "$rpkm > $count_max" | bc -l) )) ; then count_max=$rpkm ; else : ; fi
+                # If RPKM is greater than previously recorded, then overwrite the variable.
+                if (( $(echo "$rpkm > $count_max" | bc -l) )) ; then count_max=$rpkm ; else : ; fi
         
-        max=$( $samtools_exe depth -r $input_samtools $file 2>>errors.log | cut -f 3 | sort -V | tail -1 )  # Maximum Read Depth
+                max=$( $samtools_exe depth -r $input_samtools $file 2>>errors.log | cut -f 3 | sort -V | tail -1 )  # Maximum Read Depth
        
-        # If MRD is greater than previously recorded, then overwrite the variable.
-        if [[ $max -ge $max_depth ]] ; then max_depth=$max ; else : ; fi
-    done 
+                # If MRD is greater than previously recorded, then overwrite the variable.
+                if [[ $max -ge $max_depth ]] ; then max_depth=$max ; fi
 
-    [ -z "$max_depth" ] && max_depth=0                                                            # Record MRD as zero rather than blank
-    if [ -z "$count_max" ] ; then count_max=NA ; max_depth=NA ; else : ; fi                       # But if no transcription whatsoever, set everything to NA 
+            done 
 
-    echo $count_max,$max_depth >> data/$sample_type-rnaseq-$name.csv
+            [ -z "$max_depth" ] && max_depth=0      
+                                                                  # Record MRD as zero rather than blank
+            if [ -z "$count_max" ] ; then count_max=NA ; max_depth=NA ; fi                       # But if no transcription whatsoever, set everything to NA 
 
-done < $initial_data
+            echo $count_max,$max_depth >> $output_file
+
+        done 
+
+    } < $initial_data
 
 }
 
@@ -119,3 +87,5 @@ done < $initial_data
 
 run_encode 'tissue'
 run_encode 'primary-cell'
+
+paste -d',' ${output_directory}/*-tissue.csv ${output_directory}/*-primary-cell.csv > "${output_directory}/$(basename "${initial_data%.*}" | sed 's/dataset//')transcriptome.csv"  
