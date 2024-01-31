9a5b380f74972a031874f69ed9e4d6966ec95331
danischiavi <danielaschivinato@gmail.com>
2024-01-31 13:57:50 +1300

    update population-variation for snakemake

--- bin/feature-generation-splitting/population-variation-features.sh
+++ bin/feature-generation-splitting/population-variation-features.sh
@@ -13,83 +13,27 @@
 # Log files: tabix.log records any potential VCF files that weren't downloaded correctly.
 
 ############################################################################################################################
+initial_data=$1
+initial_fasta=$2
 
-####Checks 
+first_rna_id=$(awk -F',' 'NR==2 {print $1}' "$initial_data" | tr -d 'RNA')
+last_rna_id=$(awk -F',' 'END {print $1}' "$initial_data" | tr -d 'RNA') 
 
-##vcftools
-if find $additional_folder/ -executable -type f | grep -q vcftools #maybe this could be in the runall script? 
-then
-    vcftools_exe=$additional_folder/vcftools
-elif command -v vcftools &> /dev/null
-then
-    vcftools_exe=vcftools
-else
-    echo Please check that vcftools is installed.
-    #exit 1
-fi
-
-## tabix
-if find $additional_folder/ -executable -type f | grep -q tabix
-then
-    tabix_exe=$additional_folder/tabix
-elif command -v tabix &> /dev/null
-then
-    tabix_exe=tabix
-else
-    echo Please check that tabix is installed.
-    exit 1
-fi
-
-## gnomAD VCF file
-
-if [ -f $additional_folder/gnomad/gnomad.genomes.r3.0.sites.vcf.bgz ] 
-then
-    gnomad_database=$additional_folder/gnomad/gnomad.genomes.r3.0.sites.vcf.bgz
-else
-    until [ -f $gnomad_database ] ; do read -p "Please enter custom name of gnomAD SNP database (vcf.bz): " i ; gnomad_database=$addition
-al_folder/$i ; echo ; done
-fi
-
-######## Conversion file for changing coordinates between hg19 and hg38 for liftOver
-
-if [ -f $additional_folder/hg38ToHg19.over.chain ]
-then
-    chain_file=$additional_folder/hg38ToHg19.over.chain
-else
-    until [ -f $chain_file ] ; do read -p "Please enter custom name of coordinates file for converting between hg38 and Hg19 genome versions (chain): " chain ; chain_file=$additional_file/$chain ; echo ; done
-fi
-
-######## liftOver
-
-if find $additional_folder/ -executable -type f | grep -q liftOver
-then
-    liftOver_exe=$additional_folder/liftOver
-elif command -v liftOver &> /dev/null
-then
-    liftOver_exe=liftOver
-else
-    echo Please check that liftOver is installed.
-    exit 1
-fi
-
-######## tabix
-
-if find $additional_folder/ -executable -type f | grep -q tabix
-then
-    tabix_exe=$additional_folder/tabix
-elif command -v tabix &> /dev/null
-then
-    tabix_exe=tabix
-else
-    echo Please check that tabix is installed.
-    exit 1
-fi
-
-# DEFINE : $onekGP_directory
+mkdir -p data/population
+output_directory=data/population
+output_file_1kGP="${output_directory}/$(basename "${initial_data%.*}" | sed 's/dataset//')1kGP-variation.csv"                  # Define name and directory for output file
+output_file_gnomAD="${output_directory}/$(basename "${initial_data%.*}" | sed 's/dataset//')gnomAD-variation.csv" 
+ 
+# Variables
+liftOver_exe=liftOver
+chain_file=data/raw/hg38ToHg19.over.chain
+onekGP_directory=/Volumes/archive/userdata/student_users/danielaschiavinato/dani-scratch/features-of-function-data/1000genomes
+tabix_exe=tabix
+vcftools_exe=vcftools 
 
 #### File creation 
-echo 1000G_SNPs,1000G_SNPsDensity,aveMAF > data/1000g-freqsummary-$name.csv
-echo gnomAD_SNP_count,gnomAD_SNP_density,gnomAD_avg_MAF > data/gnomad-$name.csv
+echo 1000G_SNPs,1000G_SNPsDensity,aveMAF > "$output_file_1kGP"
+echo gnomAD_SNP_count,gnomAD_SNP_density,gnomAD_avg_MAF > "$output_file_gnomAD"
 
 
 
@@ -101,10 +45,14 @@ echo gnomAD_SNP_count,gnomAD_SNP_density,gnomAD_avg_MAF > data/gnomad-$name.csv
 
 #### Convert coordinates to hg19 genome version
 
-awk -F',' 'NR > 1 {print $3"\t"$4"\t"$5"\t"$1}' "$initial_data" > data/hg38-coordinates.bed     # Reformat coordinates for LiftOver
+current_coord="${output_directory}/$(basename "${initial_data%.*}" | sed 's/dataset//')hg38-coordinates.bed"
+old_coord="${output_directory}/$(basename "${initial_data%.*}" | sed 's/dataset//')hg19-coordinates.bed"
+unlifted_coord="${output_directory}/$(basename "${initial_data%.*}" | sed 's/dataset//')unlifted.bed"
 
-echo "$liftOver_exe data/hg38-coordinates.bed $chain_file data/hg19-coordinates.bed data/unlifted.bed &> /dev/null" >> errors.log
-$liftOver_exe data/hg38-coordinates.bed $chain_file data/hg19-coordinates.bed data/unlifted.bed &> /dev/null
+awk -F',' 'NR > 1 {print $3"\t"$4"\t"$5"\t"$1}' "$initial_data" > $current_coord   # Reformat coordinates for LiftOver
+
+echo "$liftOver_exe $current_coord $chain_file $old_coord $unlifted_coord &> /dev/null" >> errors.log
+"$liftOver_exe" "$current_coord" "$chain_file" "$old_coord" "$unlifted_coord" &> /dev/null
 
 ##
 
@@ -112,18 +60,20 @@ max="$last_rna_id"
 var="$first_rna_id"
 
 ## If available, unzip previously VCF files, otherwise extract files
-if [ -f data/1kGP-VCF.zip ]; then
+VCF_directory_1kGP="${output_directory}/$(basename "${initial_data%.*}" | sed 's/dataset//')1kGP-VCF" 
+    
+if [ -f "$VCF_directory_1kGP".zip ]; then
 
-    unzip data/VCF.zip &> /dev/null
+    unzip "$VCF_directory_1kGP".zip &> /dev/null
 
 else
-    mkdir -p data/1kGP-VCF
+    mkdir -p "$VCF_directory_1kGP"
     
     ## Obtain VCF files according to hg19 chromosome coordinates
-    while [ $var -le $max ]
-    do
+    while [ $var -le $max ]; do
+
         ## Grab the coordinates for each RNA as the counter increases
-        line=$( grep -w "RNA$var" data/hg19-coordinates.bed | cut -f 1,2,3 | tr '\t' ' ' | tr -d "chr" | perl -lane '{print "$F[0]:$F[1]-$F[2]"}' )
+        line=$( grep -w "RNA$var" "$old_coord" | cut -f 1,2,3 | tr '\t' ' ' | tr -d "chr" | perl -lane '{print "$F[0]:$F[1]-$F[2]"}' )
         
         ## If previous coordinate was associated with an annotated chromosome, use hg38 coordinates
         if [ -z $line ] || [[ $line == *"Un_"* ]]; then
@@ -139,46 +89,48 @@ else
 
         fi
         
-        rna_id='RNA'$var'-1k'
+        rna_id='RNA'"$var"
         chr=$( echo $line | cut -d ':' -f 1 )
 	
         ## The X chromosome is downloaded from a different folder to the autosomal chromosomes.
         if [ $chr == 'X' ]; then
         
             #until
-	        echo "$tabix_exe -f -h $onekGP_directory/ALL.chrX.phase3_shapeit2_mvncall_integrated_v1c.20130502.genotypes.vcf.gz $line > $rna_id.vcf 2>>tabix.log ;"  >> tabix.log		                                   
+	        echo "$tabix_exe -f -h "$onekGP_directory"/ALL.chrX.phase3_shapeit2_mvncall_integrated_v1c.20130502.genotypes.vcf.gz $line > $rna_id.vcf 2>>tabix.log ;"  >> tabix.log		                                   
 	        #$tabix_exe -f -h ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr$chr.phase3_shapeit2_mvncall_integrated_v1b.20130502.genotypes.vcf.gz $line > $name.vcf 2>>tabix.log ; do echo Downloading $name.vcf >> tabix.log ; echo >> tabix.log ; done
-	        $tabix_exe -f -h $onekGP_directory/ALL.chrX.phase3_shapeit2_mvncall_integrated_v1c.20130502.genotypes.vcf.gz $line > $rna_id.vcf 2>>tabix.log ;
+	        $tabix_exe -f -h "$onekGP_directory"/ALL.chrX.phase3_shapeit2_mvncall_integrated_v1c.20130502.genotypes.vcf.gz $line > $rna_id.vcf 2>>tabix.log ;
 	        #do
 	        echo Processing $rna_id.vcf >> tabix.log ; echo >> tabix.log ;
 	        #./data/1000genomes/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz
 	        #./data/1000genomes/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz
 	        #done
             ######## Tabix is very sensitive to crashing, so repeat the command until a valid VCF file has been downloaded.
+
         else
             #until
-	        echo "$tabix_exe -f -h $onekGP_directory/ALL.chr$chr.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz $line > $rna_id.vcf 2>>tabix.log ;"  >> tabix.log
+	        echo "$tabix_exe -f -h "$onekGP_directory"/ALL.chr"$chr".phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz "$line" > "$rna_id".vcf 2>>tabix.log ;"  >> tabix.log
 	        #$tabix_exe -f -h ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr$chr.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz $line > $name.vcf 2>>tabix.log ; do echo Downloading $name.vcf >> tabix.log ; echo >> tabix.log ; done
-	        $tabix_exe -f -h $onekGP_directory/ALL.chr$chr.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz $line > $rna_id.vcf 2>>tabix.log ;
+	        $tabix_exe -f -h "$onekGP_directory"/ALL.chr"$chr".phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz "$line" > "$rna_id".vcf 2>>tabix.log ;
 	        #do
-	        echo Processing $rna_id.vcf >> tabix.log ; echo >> tabix.log ;
+	        echo Processing "$rna_id".vcf >> tabix.log ; echo >> tabix.log ;
 	        #done
         fi                                                                            
 
         ######## Move VCF so it doesn't need to be redownloaded.
-        mv $rna_id.vcf data/1kGP-VCF
+        mv "$rna_id".vcf "$VCF_directory_1kGP"
         
         (( var++ ))
        
         rm -rf text.file
         
     done
+
 fi
 
 ######## Remove excess files
-rm -rf data/hg19-coordinated.bed
+#rm -rf data/hg19-coordinated.bed
 # rm -rf data/hg38-coordinates.bed
-rm -rf data/unlifted.bed
+#rm -rf data/unlifted.bed
 
 
 
@@ -195,14 +147,16 @@ VCF2summary() {
     local len="$2"
 
     ## Determine SNP frequencies for each ncRNA sequence
-    echo "$vcftools_exe --vcf $input_vcf --freq --out data/vcftools-output &> /dev/null" >> tabix.log   
-    $vcftools_exe --vcf $input_vcf --freq --out data/vcftools-output &> /dev/null
+    output_vcftools="${output_directory}/$(basename "${initial_data%.*}" | sed 's/dataset//')vcftools-output" 
+    echo "$vcftools_exe --vcf $input_vcf --freq --out $output_vcftools &> /dev/null" >> tabix.log   
+    $vcftools_exe --vcf "$input_vcf" --freq --out "$output_vcftools" &> /dev/null
 
     ## Extract SNPs for each sequence at frequency observed at that position into a parsable format
-    awk -F'\t' 'NR > 1 {print $5":"$6}' data/vcftools-output.frq > data/snps    
+    snps_file="${output_directory}/$(basename "${initial_data%.*}" | sed 's/dataset//')snps"
+    awk -F'\t' 'NR > 1 {print $5":"$6}' "$output_vcftools".frq > "$snps_file"    
 
-    SNPs_count=$( wc -l < data/snps)
-    MAF_sum=$(awk -F':' '{sum += $4} END {print sum}' data/snps)
+    SNPs_count=$( wc -l < "$snps_file")
+    MAF_sum=$(awk -F':' '{sum += $4} END {print sum}' "$snps_file")
 
     if [[ "$SNPs_count" == "0" ]] || [ -z "$SNPs_count" ]; then               # If no SNPs were recorded, set everything to NA
     
@@ -222,15 +176,15 @@ VCF2summary() {
 ## Call function for each sequence
 {
     read
-    while IFS=, read -r id _ _ start end seq                              
-    do
+    while IFS=, read -r id _ _ start end seq; do                              
+    
         len=$(( $end-$start ))                                              # to calculate SNP density
-        input_vcf="data/1kGP-VCF/$id-1k.vcf"
-        echo "[$id][$start][$end][$len][$id-1k.vcf]"  >> tabix.log  
+        input_vcf="$VCF_directory_1kGP"/"$id".vcf
+        echo "[$id][$start][$end][$len]["$id".vcf]"  >> tabix.log  
 
         VCF2summary "$input_vcf" "$len"                                     
 
-        echo "$SNPs_count,$SNPs_density,$MAF_average" >> data/1kGP-freqsummary-$name.csv
+        echo "$SNPs_count,$SNPs_density,$MAF_average" >> "$output_file_1kGP"
     
     done
    
@@ -247,9 +201,11 @@ rm -rf data/snps
 
 ############################################################################################################################
 
-mkdir -p data/VCF-gnomad/$name 
+VCF_directory_gnomAD="${output_directory}/$(basename "${initial_data%.*}" | sed 's/dataset//')gnomAD-VCF" 
 
-awk -F',' 'NR > 1 {print $3":"$4"-"$5}' "$initial_data" > data/coordinates
+coordinates="${output_directory}/$(basename "${initial_data%.*}" | sed 's/dataset//')-tabix-coordinates"
+
+awk -F',' 'NR > 1 {print $3":"$4"-"$5}' "$initial_data" > "$coordinates"
 
 ### Variables 
 max="$last_rna_id"
@@ -263,22 +219,23 @@ while IFS= read -r line; do
     start=$(echo "$line" | awk -F'[:-]' '{print $2}')
     end=$(echo "$line" | awk -F'[:-]' '{print $3}')                                 # For SNP density        len=$(( $end-$start ))  
 
-    rna_id="RNA$var-gnomad"
+    rna_id=RNA"$var"-gnomad
 
     chr=$(echo "$line" | awk -F'[:-]' '{print $1}')
-    gnomad_database=data/raw/gnomad/gnomad.genomes.v4.0.sites.$chr.vcf.bgz               # make it a variable with user input
+    gnomad_database=data/raw/gnomad/gnomad.genomes.v4.0.sites."$chr".vcf.bgz               # make it a variable with user input
 
-    echo Extracting $rna_id.vcf >> tabix.log ;
+    echo Extracting :"$rna_id".vcf >> tabix.log ;
     echo "$tabix_exe -f -h $gnomad_database $tabix_input > $rna_id.vcf 2>>tabix.log ;" >>tabix.log
-    $tabix_exe -f -h $gnomad_database $tabix_input > $rna_id.vcf 2>>tabix.log ;
+    $tabix_exe -f -h "$gnomad_database" "$tabix_input" > "$rna_id".vcf 2>>tabix.log ;
     #do
     echo >> tabix.log 
 #      done
 
-    awk -F';' '{for(i=1;i<=NF;i++) if($i ~ /^AF=/) {split($i, a, "="); print a[2]}}' $rna_id.vcf > data/snps-gnomad
+    snps_file_gnomAD="${output_directory}/$(basename "${initial_data%.*}" | sed 's/dataset//')snps-gnomAD"
+    awk -F';' '{for(i=1;i<=NF;i++) if($i ~ /^AF=/) {split($i, a, "="); print a[2]}}' "$rna_id".vcf > "$snps_file_gnomAD"
         
-    SNPs_count=$( wc -l < data/snps-gnomad)
-    MAF_sum=$(awk -F':' '{sum += $1} END {print sum}' data/snps-gnomad)
+    SNPs_count=$( wc -l < "$snps_file_gnomAD")
+    MAF_sum=$(awk -F':' '{sum += $1} END {print sum}' "$snps_file_gnomAD")
 
     if [[ "$SNPs_count" == "0" ]] || [ -z "$SNPs_count" ]; then               # If no SNPs were recorded, set everything to NA
     
@@ -293,15 +250,15 @@ while IFS= read -r line; do
     
     fi
    
-    echo "$SNPs_count,$SNPs_density,$MAF_average" >> gnomad-$name.csv 
+    echo "$SNPs_count,$SNPs_density,$MAF_average" >> "$output_file_gnomAD" 
     
     (( var++ )) 
 
-    mv $rna_id.vcf data/VCF-gnomad/$name/
+    mv "$rna_id".vcf "$VCF_directory_gnomAD"
     
-done < data/coordinates
+done < "$coordinates"
 
 
 ######## Zip VCF files for further analyses
-zip -r VCF VCF &> /dev/null
-rm -rf VCF/
+#zip -r VCF VCF &> /dev/null
+#rm -rf VCF/
