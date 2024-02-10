20036317b1dc1b144d1cd4b98a9dafcd7fe044d7
danischiavi <danielaschivinato@gmail.com>
2024-02-10 17:29:52 +1300

    update negative control

--- bin/negative-control.sh
+++ bin/negative-control.sh
@@ -1,142 +1,148 @@
+
+#### Variables and files
+initial_data=$1
+
+file_name=$(basename "${initial_data%.*}" | sed 's/coords-negative-control//')                  # Define name and directory for output file
+
+left_negative_coords=data/"$file_name"left-negative-control.csv
+right_negative_coords=data/"$file_name"right-negative-control.csv
+negative_control=data/"$file_name"negative-control-dataset.csv
+
+genome_csv=data/raw/GRCh38.p14_genome.csv
+gencode_bed=data/raw/gencode-ncrna-annotation.bed 
+uniprot_bed=data/raw/unipAliSwissprot.bed
+
+bedtools_exe=bedtools
+
+
 negative_control() {
 
-    local negative-control-position=$1
+    local distance_to_rna=$1
 
     chromo=$( echo $chr | tr -d "chr" )
-    #Generate null sequences upstream of sequence
-    left_end=$(( $start - "$negative-control-position" ))
-    left_start=$(( $left_end - $left_length ))                   # length is based on exons to match selected functional exons
+
+
+    ## Generate null sequences upstream of sequence
+
+    left_end=$(( "$start" - "$distance_to_rna" ))
+    left_start=$((   "$left_end" - "$left_len" ))                                                               # length based on exons's length
     
-    if [[ $left_end -lt 0 ]] || [[ $left_start -lt 0 ]] ; then left_sequence= ; else left_sequence=$( grep -w "chromosome $chromo" $genome_csv | cut -f 2 | cut -c$left_start-$left_end ) ; fi
+    if [[ "$left_end" -lt 0 ]] || [[ "$left_start" -lt 0 ]]; then 
+    
+        left_sequence=                                                                                          # Blank to filter out if negative coord
+
+    else 
+    
+        left_sequence=$( grep -w "chromosome $chromo" "$genome_csv" | cut -f 2 | cut -c$left_start-$left_end )  # Extract seq from genome file 
+    
+    fi
    
-    if [ -z $left_sequence ]   # If no sequence extracted, then remove.
-    then
-        :
-    elif [[ $left_sequence == *"N"* ]]   # If sequence extracted contains unknown nucleotides (N), then remove.
-    then
-        :
-    else
-        echo $chr,$left_start,$left_end,$left_sequence >> ./data/left-negative-control.csv
+    if [ ! -z $left_sequence ] && [[ $left_sequence != *"N"* ]]; then                                           # If no sequence extracted, then remove.
+
+        echo "$chr,$left_start,$left_end,$left_sequence" >> "$left_negative_coords"
+
     fi
 
-    #Generate null sequences downstream of sequence
-    right_start=$(( $end + "$negative-control-position" ))
-    right_end=$(( $right_start + $right_length ))
-    if [[ $right_start -lt 0 ]] || [[ $right_end -lt 0 ]] ; then right_sequence= ; else right_sequence=$( grep -w "chromosome $chromo" $genome_csv | cut -f 2 | cut -c$right_start-$right_end ) ; fi
+
+    ## Generate null sequences downstream of sequence
+
+    right_start=$(( "$end" + "$distance_to_rna" ))
+    right_end=$(( "$right_start" + "$right_len" ))
     
-    if [ -z $right_sequence ]     # If no sequence extracted, then remove.
-    then
-        :
-    elif [[ $right_sequence == *"N"* ]]   # If sequence extracted contains unknown nucleotides (N), then remove.
-    then
-        :
-    else
-        echo $chr,$right_start,$right_end,$right_sequence >> ./data/right-negative-control.csv
+    if [[ "$right_start" -lt 0 ]] || [[ "$right_end" -lt 0 ]]; then 
+    
+        right_sequence= 
+    
+    else 
+    
+        right_sequence=$( grep -w "chromosome $chromo" $genome_csv | cut -f 2 | cut -c$right_start-$right_end ) 
+    
+    fi
+    
+    if [ ! -z $right_sequence ] && [[ $right_sequence != *"N"* ]]; then   
+
+        echo "$chr,$right_start,$right_end,$right_sequence" >> "$right_negative_coords"
+
     fi
 
 }
 
-ncrna_bed= 
-uniprot_bed=
-###########################################################################################################################
-
-# Create negative control protein-coding sequences from human genome
 
 ###########################################################################################################################
 
-######## Generate sequences upstream and downstream of functional gene
-
-negative_control_positions=(20000, 40000, 60000, 80000, 100000)
-
-while IFS=, read chr start end left_len right_len   #start exon 1, end last exon, length exon 2 and 3
-do
-    for position in $[negative_control_positions[@]]; do
-        negative_control "$position"
-    done
-
-done < coords-for-negative-control.csv
+# Generate sequences upstream and downstream of functional gene
 
 ###########################################################################################################################
 
-# Filter upstream negative control sequences using UniProt and GENCODE
+distances_to_rna=("4000000" "8000000" "12000000" "16000000" "20000000")
+
+while IFS=, read chr start end left_len right_len; do   #start exon 1, end last exon, length exon 2 and 3
+
+    for position in "${distances_to_rna[@]}"; do negative_control "$position"; done
+
+done < "$initial_data"
+
 
 ###########################################################################################################################
 
-######## Reformat data for bedtools
-grep -v "Start" ./data/left-negative-control.csv | cut -d ',' -f 1,2,3 | tr ',' ' ' | perl -lane '{print "$F[0] $F[1] $F[2]"}' | tr ' ' '\t' > ./data/left-coordinates.bed
-
-######## Uniprot filtering
-[ -f $dep_folder/bedtools ] && $dep_folder/bedtools intersect -a $uniprot_bed -b ./data/left-coordinates.bed > ./data/left-intersect.bed || bedtools intersect -a $uniprot_bed -b ./data/left-coordinates.bed > ./data/left-intersect.bed
-
-######## Extract chromosome coordinates of overlapping negative control sequences
-cat ./data/left-intersect.bed | cut -f 1,2,3 | tr -d "chr" | tr '\t' ',' > ./data/left-intersect
-left_count=$( cat ./data/left-intersect | wc -l )
-
-######## GENCODE filtering
-[ -f $dep_folder/bedtools ] && $dep_folder/bedtools intersect -a $ncrna_bed -b ./data/left-coordinates.bed > ./data/left-intersect.bed || bedtools intersect -a $ncrna_bed -b ./data/left-coordinates.bed > ./data/left-intersect.bed
-
-######## Extract chromosome coordinates of overlapping negative control sequences
-cat ./data/left-intersect.bed | cut -f 1,2,3 | tr -d "chr" | tr '\t' ',' > ./data/left-intersect
-
-######## Remove negative control sequences that overlap with known functional sequences
-echo ID,Functional,Chromosome,Start,End,Sequence > ./data/negative-control-dataset.csv
-count=$( tail -1 ./data/protein-exon2-dataset.csv | cut -d ',' -f 1 | tr -d "RNA" )
-
-for line in $( cat ./data/left-negative-control.csv )
-do
-    coordinates=$( echo $line | cut -d ',' -f 1,2,3 | tr -d "chr" )
-
-    if grep -q $coordinates ./data/left-intersect               # If overlap with protein-coding gene
-    then
-        :
-    elif grep -q $coordinates ./data/left-intersect            # If overlap with ncRNA gene
-    then
-        :
-    else
-        (( count++ ))
-        echo RNA$count,No,$line >> ./data/negative-control-dataset.csv
-    fi
-done
+# Filter upstream (left) and downstream (right) negative control sequences using UniProt and GENCODE
 
 ###########################################################################################################################
 
-# Filter downstream negative control sequences using UniProt and GENCODE
+filter_out_functional(){
+    
+    local side=$1
 
-###########################################################################################################################
+    ## Temporary files
+    negative_coords=data/${file_name}${side}-negative-control.csv
+    bed_coords=data/${file_name}${side}-coordinates.bed
+    bed_intersect_uniprot=data/${file_name}${side}-intersect-uniprot.bed
+    bed_intersect_gencode=data/${file_name}${side}-intersect-gencode.bed
+    intersect_uniprot=data/${file_name}${side}-intersect-uniprot
+    intersect_gencode=data/${file_name}${side}-intersect-gencode
 
-######## Reformat data for bedtools
-grep -v "Start" ./data/right-negative-control.csv | cut -d ',' -f 1,2,3 | tr ',' ' ' | perl -lane '{print "$F[0] $F[1] $F[2]"}' | tr ' ' '\t' > ./data/right-coordinates.bed
+    ######## Reformat data for bedtools
+    grep -v "Start" "$negative_coords" | awk -F',' '{print $1 "\t" $2 "\t" $3}' > "$bed_coords"
 
-######## Uniprot filtering
-[ -f $dep_folder/bedtools ] && $dep_folder/bedtools intersect -a $uniprot_bed -b ./data/right-coordinates.bed > ./data/right-intersect.bed || bedtools intersect -a $uniprot_bed -b $d-right-coordinates.bed > $d-right-intersect.bed
+    ######## Uniprot filtering
+    "$bedtools_exe" intersect -a "$uniprot_bed" -b "$bed_coords" > "$bed_intersect_uniprot"
 
-######## Extract chromosome coordinates of overlapping negative control sequences
-cat ./data/right-intersect.bed | cut -f 1,2,3 | tr -d "chr" | tr '\t' ',' > ./data/right-intersect
-right_count=$( cat ./data/right-intersect | wc -l )
+    ######## Extract chromosome coordinates of overlapping negative control sequences
+    cut -f 1-3 "$bed_coords" | sed 's/^chr//' | tr '\t' ',' > "$intersect_uniprot"
+    left_count=$( cat "$intersect" | wc -l )
 
-######## GENCODE filtering
-[ -f $dep_folder/bedtools ] && $dep_folder/bedtools intersect -a $ncrna_bed -b ./data/right-coordinates.bed > ./data/right-intersect.bed || bedtools intersect -a $ncrna_bed -b ./data/right-coordinates.bed > ./data/right-intersect.bed
+    ######## GENCODE filtering
+    "$bedtools_exe" intersect -a "$gencode_bed" -b "$bed_coords" > "$bed_intersect_gencode"
 
-######## Extract chromosome coordinates of overlapping negative control sequences
-cat ./data/right-intersect.bed | cut -f 1,2,3 | tr -d "chr" | tr '\t' ',' > ./data/right-intersect
+    ######## Extract chromosome coordinates of overlapping negative control sequences
+    cut -f 1-3 "$bed_coords" | sed 's/^chr//' | tr '\t' ',' > "$intersect_gencode"
 
-for line in $( cat ./data/right-negative-control.csv )
-do
-    coordinates=$( echo $line | cut -d ',' -f 1,2,3 | tr -d "chr" )
+    ######## Remove negative control sequences that overlap with known functional sequences
+    echo "ID,Functional,Chromosome,Start,End,Sequence" > "$negative_control"
 
-    if grep -q $coordinates ./data/right-intersect      # If overlap with protein-coding gene
-    then
-        :
-    elif grep -q $coordinates ./data/right-intersect       # If overlap with ncRNA gene
-    then
-        :
-    else
-        (( count++ ))
-        echo RNA$count,No,$line >> ./data/negative-control-dataset.csv
-    fi
-done
+    count=$(awk -F',' 'END {print $1}' "$initial_data" | tr -d 'RNA')                           # Start counting from last functional seq  
+
+    while IFS=',' read -r chr start end _; do
+
+        coordinates="${chr}${start}${end}"
+
+        if grep -qF "$coordinates" "$intersect_uniprot"; then
+            :
+        elif grep -qF "$coordinates" "$intersect_gencode"; then
+            :
+        else
+            (( count++ ))
+            echo "RNA$count,No,$chr,$start,$end" >> "$negative_control"
+        fi
+
+    done < "$negative_coords"
+
+}
+
+filter_out_functional 'left'
+filter_out_functional 'right'
 
 ######## Generate negative control FASTA file
-grep -v "Start" ./data/negative-control-dataset.csv | cut -d ',' -f 1,6 | tr ',' ' ' | perl -lane '{print ">$F[0]\n$F[1]"}' > ./data/negative-control-seq.fa
+#grep -v "Start" ./data/negative-control-dataset.csv | cut -d ',' -f 1,6 | tr ',' ' ' | perl -lane '{print ">$F[0]\n$F[1]"}' > ./data/negative-control-seq.fa
 
 ###########################################################################################################################
