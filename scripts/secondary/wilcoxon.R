8c6792730677ab06460215e05bc810a60494a03d
danischiavi <danielaschivinato@gmail.com>
2024-03-11 20:05:27 +1300

    include secondary script folder

--- scripts/secondary/wilcoxon.R
+++ scripts/secondary/wilcoxon.R
@@ -1,37 +1,40 @@
-
-args <- commandArgs(trailingOnly = TRUE)
-file <- args[1]
-output_file <- args[2]
+file <- commandArgs(trailingOnly = TRUE)
 
 options(repos = "https://cloud.r-project.org")
-install.packages("RVAideMemoire")
-library(RVAideMemoire)
 install.packages("randomForest")
 library(randomForest)
+install.packages(tools)
+library(tools)
 
-# 1. Read the data from the file into a data frame
-spearman_correlation <- function(file){
+# Format output file 
+filename <- sub("features\\.csv$", "", file)
+output_file <- paste0(filename,"wilcoxon-matrix")
 
+
+# Function declaration to calculate Rho spearman and its confidence intervals 
+wilcoxon_test <- function(file){
+  
   data <- read.csv(file, header=TRUE)
   
   # Convert Functional "Yes" "No" into numeric values 
-  data$Functional <- factor(data$Functional, levels=c("Yes","No")) 
+  data$Functional <- factor(data$Functional, levels=c("No","Yes")) 
   functional_numeric <- as.numeric(factor(data$Functional)) - 1 
-
+  
   # Approx NA values (CHECK method) 
   data <- na.roughfix(data)
-
+  
   # 2. Extract the other numeric columns
   numeric_data <- data[, !names(data) %in% c("Functional")]
-
+  
   # Calculate the Spearman correlation between "functional" column and each numeric column
   correlations <- sapply(numeric_data, function(col) {
-    spearman <- spearman.ci(functional_numeric, col, conf.level = 0.95, nrep = 1000)
-    rho <- spearman$estimate[[1]]
-    ci_inf <- spearman$conf.int[[1]] 
-    ci_sup <- spearman$conf.int[[2]]
-    return(c( cor = rho, ci_inf = ci_inf, ci_sup = ci_sup ))
-  })
+    wilcoxon <- wilcox.test(functional_numeric, col, conf.level = 0.95, conf.int = TRUE)
+    W <- wilcoxon$statistic[[1]]
+    p_value <- wilcoxon$p.value[1]
+    ci_inf <- wilcoxon$conf.int[[1]] 
+    ci_sup <- wilcoxon$conf.int[[2]]
+    return(c( cor = W, pvalue = p_value, ci_inf = ci_inf, ci_sup = ci_sup ))
+    })
   
   # Return correlation coefficients and confidence intervals as a named list with variable name as the name
   return(correlations)
@@ -41,8 +44,8 @@ spearman_correlation <- function(file){
 # Initialize empty list to store correlation vectors
 correlation_list <- list()
 
-correlation_list[[file]] <- spearman_correlation(file)
-  
+correlation_list[[file]] <- wilcoxon_test(file)
+
 correlation_matrix_combined <- do.call(rbind, correlation_list)
 
 # Write the correlation matrix to a CSV file
