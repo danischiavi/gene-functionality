6cdcf7d9351dd16829a973c0bd8eadd9c2535d2b
danischiavi <danielaschivinato@gmail.com>
2024-03-11 20:00:29 +1300

    update maf-to-stk to pad partial alignm with 'N'

--- scripts/maf-to-stk.sh
+++ scripts/maf-to-stk.sh
@@ -22,7 +22,6 @@ output_file="${stk_directory}/$(basename "${input_file%.*}" | sed 's/maf//').stk
 
 
 ## Array with species Ids present in the MAF alignment 
-
 declare -a "species_id=()"
 mapfile -t species_id < <(grep -v -E "#|score=[[:alnum:]]|^$" "$input_file" | awk '{print $2}' | awk '!seen[$0]++')    
 
@@ -46,24 +45,41 @@ for species_id in "${species_id[@]}"; do
 done
 
 
-## Remove partial aligments whose length is less than 50% of human's (to calculate length: '-' and 'N' are not included)
+## Remove partial alignments < 50%: determinated by length less than 50% of human's not including '-' and 'N'
+# Pad included partial alignments with 'N' to match human's len for further analysis with tools 
+
 len_hg38=$(awk -v partial_id="hg38" '{ if ($1 ~ "^" partial_id) { print length($2) } }' "$output_file".tmp)
 
-awk -v len="$len_hg38" '
+awk -v len="$len_hg38" -v longest_id="$longest_id" '
     function custom_length(str) {
         gsub("-", "", str);                                         
         gsub("N", "", str) 
         return length(str); 
     } 
+
+    function pad_sequence(seq, target_len, longest_id) {
+        sequence_length = length(seq)
+        padding_length = target_len - sequence_length
+        if (padding_length > 0) {
+            padding = sprintf("%*s", padding_length, "")
+            gsub(/ /, "N", padding)
+            return seq padding
+        } else {
+            return seq
+        }
+    }
+
     { 
-        if (custom_length($2) >= len * 0.5) 
-            print 
+        if (custom_length($NF) >= len * 0.5 && length($NF) <= len) {
+            sequence = pad_sequence($NF, len, longest_id)
+            printf "%-*s\t%s\n", longest_id, $1, sequence
+        }   
     }' "$output_file".tmp > "$output_file".tmp2
 
 
 ## Format stk file
 echo -e "# STOCKHOLM 1.0 \n#=GF ID\t$id" > "$output_file"
-awk '!seen[$2]++' "$output_file".tmp2 >> "$output_file"
+awk '!seen[$2]++' "$output_file".tmp2 >> "$output_file"         # Remove duplicated sequences
 echo "//" >> "$output_file" 
 
 rm -rf "$output_file".tmp
