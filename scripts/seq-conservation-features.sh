32d48cccfa8a5aaebe8f430a99d863a76017ffb3
danischiavi <danielaschivinato@gmail.com>
2024-03-20 07:05:54 +1300

    remove 100w phyloP

--- scripts/seq-conservation-features.sh
+++ scripts/seq-conservation-features.sh
@@ -2,7 +2,7 @@
 #
 # Script Name: seq-conservation-features.sh
 #
-# Description: This script calculates the PhyloP, PhyloCons and GERP for for protein-coding exons, lncRNA exons, short ncRNAs 
+# Description: This script calculates the PhyloP (based on 241way-alignment of Zoonomia project), PhastCons and GERP (based on 100way vertebrate alignment) for for protein-coding exons, lncRNA exons, short ncRNAs 
 #              and negative control sequences.
 #
 # Input: $1 is the dataset 
@@ -15,10 +15,9 @@
 #### Files and directories #### 
 initial_data=$1
 bigWigSummary_exe=$2
-phylo_bw=$3
-phast_bw=$4
-zoonomia_phylo_bw=$5
-gerp_bw=$6
+phast_bw=$3
+zoonomia_phylo_bw=$4
+gerp_bw=$5
 
 output_directory=data/conservation
 mkdir -p "$output_directory"
@@ -26,30 +25,33 @@ mkdir -p "$output_directory"
 output_file="${output_directory}/$(basename "${initial_data%.*}" | sed 's/dataset//')conservation.csv"                  
 
 
-#### Variables and databases #### --> D: still not sure which is the best way to handle this inputs . Some are relative paths to the directory of the project and other are absolute 
-
-
-
 #### Extract conservation features for each set of chromosome coordinates #### 
 if [ ! -s "$output_file" ]; then
 
-    echo "241w_PP_mean,241w_PP_max,100w_PP_mean,100w_PP_max,100w_PC_mean,100w_PC_max,GERP_mean,GERP_max" > "$output_file"
+    echo "phyloP_mean,phyloP_max, phastCons_mean,phastCons_max,GERP_mean,GERP_max" > "$output_file"
 
     tail -n +2 "$initial_data"  | while IFS=',' read -r _ _ chr start end _; do
 
-        ##### phastCons (pc) and phyloP (pp) values: 100way-alignment #####
-	    echo "mean_pp = $bigWigSummary_exe -type=mean $phylo_bw $chr $start $end 1 2>&1" >> errors.log
-        mean_pp=$( $bigWigSummary_exe -type=mean $phylo_bw $chr $start $end 1 2>&1 )
-        max_pp=$(   $bigWigSummary_exe -type=max $phylo_bw $chr $start $end 1 2>&1 )
-        mean_pc=$( $bigWigSummary_exe -type=mean $phast_bw $chr $start $end 1 2>&1 )
-        max_pc=$(   $bigWigSummary_exe -type=max $phast_bw $chr $start $end 1 2>&1 )
-	    echo "max_pc = $bigWigSummary_exe -type=max $phast_bw $chr $start $end 1 2>&1" >> errors.log
-	
         #### PhyloP (pp) values: 241-way mammalian alignment ####
+        echo "zoonomia_mean=$( $bigWigSummary_exe -type=mean $zoonomia_phylo_bw $chr $start $end 1 2>&1 )" >> errors.log
+        echo "zoonomia_max=$(   $bigWigSummary_exe -type=max $zoonomia_phylo_bw $chr $start $end 1 2>&1 )" >> errors.log
+        
         zoonomia_mean=$( $bigWigSummary_exe -type=mean $zoonomia_phylo_bw $chr $start $end 1 2>&1 )
         zoonomia_max=$(   $bigWigSummary_exe -type=max $zoonomia_phylo_bw $chr $start $end 1 2>&1 )
-    
-        #### Obtain GERP Scores: 100w-alignment ####
+        
+        
+        ##### phastCons (pc) values: 100way-alignment #####
+        echo "max_pc =   $bigWigSummary_exe -type=max $phast_bw $chr $start $end 1 2>&1" >> errors.log
+        echo "mean_pc = $bigWigSummary_exe -type=mean $phast_bw $chr $start $end 1 2>&1" >> errors.log
+        
+        mean_pc=$( $bigWigSummary_exe -type=mean $phast_bw $chr $start $end 1 2>&1 )
+        max_pc=$(   $bigWigSummary_exe -type=max $phast_bw $chr $start $end 1 2>&1 )
+	    
+
+        #### GERP Scores: 100way-alignment ####
+        echo "gerp_mean = $bigWigSummary_exe -type=mean $gerp_bw $chr $start $end 1 2>&1" >> errors.log
+        echo "gerp_max =   $bigWigSummary_exe -type=max $gerp_bw $chr $start $end 1 2>&1" >> errors.log
+
         gerp_mean=$( $bigWigSummary_exe -type=mean $gerp_bw $chr $start $end 1 2>&1 )
         gerp_max=$(   $bigWigSummary_exe -type=max $gerp_bw $chr $start $end 1 2>&1 )
 
@@ -66,8 +68,6 @@ if [ ! -s "$output_file" ]; then
             echo "$var"
         }
 
-        mean_pp=$(missing_value "$mean_pp")
-        max_pp=$(missing_value "$max_pp")
         mean_pc=$(missing_value "$mean_pc")
         max_pc=$(missing_value "$max_pc")
         zoonomia_mean=$(missing_value "$zoonomia_mean")
@@ -76,7 +76,7 @@ if [ ! -s "$output_file" ]; then
         gerp_max=$(missing_value "$gerp_max")
 
         ## Values to output file 
-        echo "$zoonomia_mean,$zoonomia_max,$mean_pp,$max_pp,$mean_pc,$max_pc,$gerp_mean,$gerp_max" >> "$output_file"
+        echo "$zoonomia_mean,$zoonomia_max,$mean_pc,$max_pc,$gerp_mean,$gerp_max" >> "$output_file"
 
     done
 
