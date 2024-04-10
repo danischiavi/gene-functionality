1c7e3e07a10564ec4afa67e98de4a4e8f63ac70b
danischiavi <danielaschivinato@gmail.com>
2024-04-10 15:44:55 +1200

    include numbers on plot

--- complementary/heatmap-features.R
+++ complementary/heatmap-features.R
@@ -1,8 +1,11 @@
 #### FEATURES CORRELATION MATRIX - FEATURE:FEATURE (for each type of RNA individually) ####
 
-# file <- "/Users/danischiavinato/Desktop/data-from-server/20240320/short-features"
+# file <- "/Users/danischiavinato/Desktop/data-from-server/20240320/protein-features"
 
 data <- read.csv(file, header = TRUE)
+library(ggplot2)
+library(reshape2)
+library(randomForest)
 
 data$Functional <- factor(data$Functional, levels=c("No","Yes")) 
 functional_numeric <- as.numeric(factor(data$Functional)) - 1 
@@ -12,23 +15,21 @@ data <- na.roughfix(data)
 cor_matrix <- cor(data, method = "spearman")
 
 # Heatmap
-library(ggplot2)
-library(reshape2)
-
 # Melt the correlation matrix into long format
 cor_melted <- melt(cor_matrix)
 
 ggplot(cor_melted, aes(Var1, Var2, fill = value)) +
   geom_tile() +
+  geom_text(aes(label = round(value, 2)), color = "black") +
   scale_fill_gradient2(low = "blue", mid = "white", high = "red", 
                        midpoint = 0, limits = c(-1, 1), name = "Spearman Correlation") +
   theme(
     axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
-    plot.title = element_text(size = 12),  # Increase title size
+    plot.title = element_text(size = 20),  # Increase title size
     axis.title = element_text(size = 10),  # Increase axis title size
     axis.text = element_text(size = 20),    # Increase axis label size
     legend.title = element_text(size = 10),  # Increase legend title size
     legend.text = element_text(size = 10),   # Increase legend text size
     legend.key.size = unit(2, "lines")       # Increase legend key size
   ) +
-  labs(title = "Features Spearman Correlation - short-ncRNA", x = "Features", y = "Features")
+  labs(title = "Features Spearman Correlation - protein-coding-RNA", x = "Features", y = "Features")
