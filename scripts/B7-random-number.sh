25884f6174dbb7e881463a3037ce9f8ffafe4843
danischiavi <danielaschiavinato@gmail.com>
2024-07-25 13:52:36 +1200

    add

---
+++ scripts/B7-random-number.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+input_file=$1
+
+output_directory=data/neutral
+mkdir -p "$output_directory"
+
+file_name=${output_directory}/$(basename "${input_file%.*}" | sed 's/-dataset//')
+output_file="$file_name"-random.csv 
+
+
+### RANDOM NUMBER - NEUTRAL PREDICTOR ###
+
+total="$(wc -l < ${input_file})"
+
+echo 'Neutral' > "$output_file" 
+
+i=1
+while [ "$i" -lt "$total" ]; do 
+  echo $RANDOM >> "$output_file"
+  (( i++))
+done 
+
